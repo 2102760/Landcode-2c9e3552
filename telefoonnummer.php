a1e4d6ddb5a137d75bfd8fe82ff1446ecb87f854
Beau van Rouwendal <2102760@talnet.nl>
2020-06-19 10:38:00 +0200

    uitwerking

--- telefoonnummer.php
+++ telefoonnummer.php
@@ -1,15 +1,19 @@
 <?php
 
 // Args
+if (!isset($argv[1])) {
+    die('Hahaha jij denke dit werke zonder telefoonnummer dacht het niet!');
+}
 $telefoonnummer = $argv[1];
-echo "";
 
 // Regex check
-$regex = "/^[0-9]{10}$/";
-preg_match($regex, $telefoonnummer, $match);
+$regexWOL = "/[0-9]{10}/m";
+$regexWL = "/[+][0-9]{11}/m";
+preg_match($regexWL, $telefoonnummer, $matchWL);
+preg_match($regexWOL, $telefoonnummer, $matchWOL);
 
 // Result
-if (isset($match[0]) && $match[0] === $telefoonnummer) {
+if (isset($matchWL[0]) && $matchWL[0] === $telefoonnummer || $matchWOL[0] === $telefoonnummer) {
     echo "Het ingevoerde telefoonnummer is: '{$telefoonnummer}' en dat is juist";
 } else {
     echo "Het ingevoerde telefoonnummer is: '{$telefoonnummer}' en dat is onjuist";
